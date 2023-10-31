dceb5d77782c6247116c90983a318480d51d5bbd
k <Kylel@email.arizona.edu>
2023-10-30 22:43:02 -0700

    menu half done

--- main.c
+++ main.c
@@ -6,16 +6,96 @@
 #include "ShoppingCart.c"
 #include "ShoppingCart.h"
 
+void PrintMenu(){
+   printf("MENU\n");
+   printf("a - Add item to cart\n");
+   printf("r - Remove item from cart\n");
+   printf("c - Change item quantity\n");
+   printf("i - Output items' descriptions\n");
+   printf("o - Output shopping cart\n");
+   printf("q - Quit\n");
+   printf("\n");
+   printf("Choose an option:\n");
+   return;
+}
+
+ShoppingCart ExecuteMenu(char option, ShoppingCart SC){
+   
+       switch(option)
+       {
+          case 'a': //add item to cart
+             char s;
+             int price, qty;
+             ItemToPurchase ItP;
+             
+             printf("ADD ITEM TO CART\nEnter the item name:\n");
+             fflush(stdin);
+             fgets(ItP.itemName, sizeof(ItP.itemName), stdin);
+             for(int i=0; i<strlen(ItP.itemName); i++){//make sure it is a string
+                if(ItP.itemName[i]=='\n'){
+                   ItP.itemName[i] = '\0';
+                }
+             }
+             printf("Enter the item description:\n");
+             fflush(stdin);
+             fgets(ItP.itemDescription, sizeof(ItP.itemDescription), stdin);
+             for(int i=0; i<strlen(ItP.itemDescription); i++){//make sure it is a string
+                if(ItP.itemDescription[i]=='\n'){
+                   ItP.itemDescription[i] = '\0';
+                }
+             }
+             printf("Enter the item price:\n");
+             scanf("%d", &ItP.itemPrice);
+             printf("Enter the item quantity:\n");
+             scanf("%d", &ItP.itemQuantity);//now push it
+             if(SC.cartSize!=10){SC.cartItems[SC.cartSize] = ItP; SC.cartSize++;}
+             else{printf("CART IS FULL\n");}
+          break;
+          case 'r': break;
+          case 'c': break;
+          case 'i': break;//descriptions
+          case 'o': break;//shopping cart
+          
+       }
+   return SC;
+}
 
 int main(void) {
   
  
  
-   ///*Appendix- The following is part 1  
+    
    const int maxbuf =50;
-   int price1, price2, qty1, qty2;// allocA, allocB;
-   char buf1[maxbuf], buf2[maxbuf], c, s[maxbuf];
+   int price1, price2, qty1, qty2;
+   char buf1[maxbuf], buf2[maxbuf], c='y', s; 
+   ShoppingCart SC;
+   printf("Enter customer's name:\n");
+   fflush(stdin);
+   fgets(SC.customerName, sizeof(SC.customerName), stdin);
+   SC.customerName[strlen(SC.customerName)-1] = '\0'; //remove the new line that gets captured
+   printf("Enter today's date:\n");
+   scanf("%s", SC.currentDate);
+   printf("\nCustomer name: %s\nToday's date: %s\n", SC.customerName, SC.currentDate);
+   //require an aditional new line to begin the next item entry
+   c = getchar();
+   while(c != '\n' && c != EOF) {
+      c = getchar();
+   }
    
+   while(c != 'q'){
+      PrintMenu();
+      
+      scanf("%c", &c);
+      s = getchar();
+      while(s != '\n' && s != EOF) { s = getchar(); }//filter the extra new line
+      
+      SC = ExecuteMenu(c, SC);
+      
+   }
+   /*
+   //
+   ///*Appendix- The following is part 1 and testing
+   //
    printf("Item 1\nEnter the item name:\n");
    fflush(stdin);
    fgets(buf1, sizeof(buf1), stdin);
@@ -70,7 +150,7 @@ int main(void) {
    PrintItemCost(B);
    printf("\nTotal: $%d\n", (A.itemPrice) * (A.itemQuantity) + (B.itemPrice) * (B.itemQuantity));
    //Testing Add Item
-   ShoppingCart SC;
+   
    SC = AddItem(A, SC);
    SC = AddItem(B, SC);
    
@@ -98,6 +178,7 @@ int main(void) {
    //PrintItemDescription(A);
    //PrintItemDescription(B);
    //free(A);
-   //free(B);*/
+   //free(B);
+   */
    return 0;
 }
